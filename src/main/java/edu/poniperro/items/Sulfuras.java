6fcc8609a8199d6a3f6a6c9a9e4c22485a4102a1
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-04 16:20:46 +0100

    [✨] implementar Sulfuras

---
+++ src/main/java/edu/poniperro/items/Sulfuras.java
@@ -0,0 +1,14 @@
+package edu.poniperro.items;
+
+public class Sulfuras extends Item{
+
+    //Contructor
+    public Sulfuras (String name, int quality, int sellIn) {
+        super(name, quality, sellIn);
+    }
+
+    @Override
+    public void update_q(){
+
+    }
+}
